520406a3830a8ee70839cd78b55b7cc10b301772
CAMINOonPC <35667100+CAMINOonPC@users.noreply.github.com>
2020-06-08 16:38:13 +0300

    Added missing mods to offline batch files

--- Missions/ExpansionCOM.ChernarusPlus/DayZCommunityOfflineMode.bat
+++ Missions/ExpansionCOM.ChernarusPlus/DayZCommunityOfflineMode.bat
@@ -6,4 +6,4 @@ RD /s /q "storage_-1" > nul 2>&1
 
 cd ../../
 
-start DayZ_x64.exe -mission=.\Missions\ExpansionCOM.ChernarusPlus -nosplash -noPause -noBenchmark -filePatching -doLogs -scriptDebug=true "-mod=!Workshop\@DayZ-Expansion;!Workshop\@CF;!Workshop\@Community-Online-Tools;"
+start DayZ_x64.exe -mission=.\Missions\ExpansionCOM.ChernarusPlus -nosplash -noPause -noBenchmark -filePatching -doLogs -scriptDebug=true "-mod=!Workshop\@DayZ-Expansion;!Workshop\@DayZ-Expansion-Licensed;!Workshop\@CF;!Workshop\@Community-Online-Tools;"
